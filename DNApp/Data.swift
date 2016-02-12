4e38c8c8751f48542da7de2a6e6616a15fd0217d
Arnold Sandoval <arnoldsandoval@me.com>
2016-02-12 15:34:26 -0800

    adds static data

---
+++ DNApp/Data.swift
@@ -0,0 +1,141 @@
+//
+//  Data.swift
+//  DNApp
+//
+//  Created by Arnold Sandoval on 2/12/16.
+//  Copyright © 2016 Arnold Sandoval. All rights reserved.
+//
+
+import UIKit
+
+var data: [[String:AnyObject]] = [
+    [
+        "id": 63854,
+        "title": "UX Design for Women",
+        "comment": "",
+        "comment_html": "",
+        "comment_count": 0,
+        "vote_count": 4,
+        "created_at": "2016-02-12T18:30:10Z",
+        "pinned_at": "",
+        "url": "https://www.designernews.co/click/stories/63854",
+        "site_url": "https://www.designernews.co/stories/63854-ux-design-for-women",
+        "user_id": 36347,
+        "user_display_name": "Sheta C.",
+        "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/36347/original/portrait-2015-12-16_22_59_43__0000-3634720151216-3-1pfedns.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486868240&Signature=rxeP%2ByeTETziB2ixKz84wzsbgDc%3D",
+        "hostname": "goo.gl",
+        "user_url": "http://www.designernews.co/users/36347/sheta-chatterjee",
+        "badge": "",
+        "user_job": "Product Designer",
+        "sponsored": false,
+        "comments": [
+            [
+                "id": 202855,
+                "body": "Thank you! This happened to me today and I had to clue what's causing it, thought my drive failed!",
+                "body_html": "<p>Thank you! This happened to me today and I had to clue what&#39;s causing it, thought my drive failed!</p>\n",
+                "created_at": "2016-02-12T15:29:19Z",
+                "depth": 0,
+                "vote_count": 3,
+                "url": "https://api.designernews.co/comments/202855",
+                "user_url": "http://www.designernews.co/users/8104/mahdi-alfarra",
+                "user_id": 8104,
+                "user_display_name": "Mahdi A.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/8104/original/portrait-2015-10-06_13_53_43__0000-810420151006-3-mo8bvh.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486864918&Signature=J1jM0OTuoNtJWo6f2hBHmDTlHBE%3D",
+                "user_job": "UX/UI Designer @ NGTI"
+
+            ],
+            [
+                "id": 202884,
+                "body": "[Here's a video of it happening](https://www.youtube.com/watch?v=0xKlFO3_j5E).",
+                "body_html": "<p><a href=\"https://www.youtube.com/watch?v=0xKlFO3_j5E\" rel=\"nofollow\">Here&#39;s a video of it happening</a>.</p>\n",
+                "created_at": "2016-02-12T17:24:29Z",
+                "depth": 0,
+                "vote_count": 1,
+                "url": "https://www.designernews.co/comments/202884",
+                "user_url": "http://www.designernews.co/users/3673/david-barker",
+                "user_id": 3673,
+                "user_display_name": "David B.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/3673/original/portrait-2015-05-30_03_27_54__0000-367320150530-3-aqevws.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486866747&Signature=kNyF%2B1IbRrycS824hv8DQxiwWxo%3D",
+                "user_job": "Designer / Developer"
+            ],
+            [
+                "id": 202898,
+                "body": "The Creative Cloud app does all manner of nonsense that nobody wants. One of the many reasons I cancelled.\r\n\r\nRemember when you just installed software and ran it, without a nanny app that checked up on you to ensure you were doing everything according to the rules? Pepperidge Farm remembers.",
+                "body_html": "<p>The Creative Cloud app does all manner of nonsense that nobody wants. One of the many reasons I cancelled.</p>\n\n<p>Remember when you just installed software and ran it, without a nanny app that checked up on you to ensure you were doing everything according to the rules? Pepperidge Farm remembers.</p>\n",
+                "created_at": "2016-02-12T18:44:35Z",
+                "depth": 0,
+                "vote_count": 4,
+                "url": "https://api.designernews.co/comments/202898",
+                "user_url": "http://www.designernews.co/users/17198/daniel-de-laney",
+                "user_id": 17198,
+                "user_display_name": "Daniel D.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/17198/original/portrait-2015-10-29_19_17_18__0000-1719820151029-3-yv5c47.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486871702&Signature=NiASeozJHs0H6UqHwxElNJKYCqQ%3D",
+                "user_job": "Designer"
+            ]
+        ]
+    ],
+    [
+        "id": 63842,
+        "title": "Site Design: Authentic Jobs",
+        "comment": "",
+        "comment_html": "",
+        "comment_count": 10,
+        "vote_count": 13,
+        "created_at": "2016-02-12T15:40:56Z",
+        "pinned_at": "",
+        "url": "https://www.designernews.co/click/stories/63842",
+        "site_url": "https://www.designernews.co/stories/63842-site-design-authentic-jobs",
+        "user_id": 4182,
+        "user_display_name": "Pedro P.",
+        "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/4182/original/portrait-2015-10-09_19_31_52__0000-418220151009-3-b7gwop.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486874141&Signature=W%2FcbGsJMxxAwT7EtW12SRgCsbxo%3D",
+        "hostname": "authenticjobs.com",
+        "user_url": "http://www.designernews.co/users/4182/pedro-pinto",
+        "badge": "design",
+        "user_job": "Front End and Design freelancer",
+        "sponsored": false,
+        "comments": [
+            [
+                "id": 202869,
+                "body": "It's nice but I'll definitely miss their gradient buttons. They were one of the hold outs and it made me happy.",
+                "body_html": "<p>It&#39;s nice but I&#39;ll definitely miss their gradient buttons. They were one of the hold outs and it made me happy.</p>\n",
+                "created_at": "2016-02-12T16:45:32Z",
+                "depth": 0,
+                "vote_count": 2,
+                "url": "https://www.designernews.co/comments/202869",
+                "user_url": "http://www.designernews.co/users/12180/steven-cavins",
+                "user_id": 12180,
+                "user_display_name": "Steven C.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/12180/original/portrait-2015-07-02_18_54_46__0000-1218020150702-3-nt2r1h.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486874142&Signature=KYkeVvkAP%2BnM5v8IkILDZNIzevQ%3D",
+                "user_job": "User Interface Designer"
+            ],
+            [
+                "id": 202892,
+                "body": "Not sure if I'm feeling the new site, with the 100% width layout it feels like a giant spreadsheet.  Maybe I'm just resistant to change.",
+                "body_html": "<p>Not sure if I&#39;m feeling the new site, with the 100% width layout it feels like a giant spreadsheet.  Maybe I&#39;m just resistant to change.</p>\n",
+                "created_at": "2016-02-12T17:58:49Z",
+                "depth": 0,
+                "vote_count": 0,
+                "url": "https://api.designernews.co/comments/202892",
+                "user_url": "http://www.designernews.co/users/8871/philip-lester",
+                "user_id": 8871,
+                "user_display_name": "Philip L.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/8871/original/portrait-2015-05-30_03_49_53__0000-887120150530-3-mn3zm9.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486866779&Signature=VgKL918bNhHCW1XSufsCxfXte0I%3D",
+                "user_job": "Founder & Creative Director @ Dreamten"
+            ],
+            [
+                "id": 202897,
+                "body": "This is going to be taken down in 3... 2... 1...",
+                "body_html": "<p>This is going to be taken down in 3... 2... 1...</p>\n",
+                "created_at": "2016-02-12T18:37:52Z",
+                "depth": 0,
+                "vote_count": 0,
+                "url": "https://www.designernews.co/comments/202897",
+                "user_url": "http://www.designernews.co/users/3610/randall-morris",
+                "user_id": 3610,
+                "user_display_name": "Randall M.",
+                "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/3610/original/portrait-2015-05-30_03_27_35__0000-361020150530-3-1u26txa.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ&Expires=1486869128&Signature=Vja1B7ejXcBhlLNQ8pIouTxCzrQ%3D",
+                "user_job": "Design Director @ CBS Fantasy Sports"
+            ]
+        ]
+    ]
+]
