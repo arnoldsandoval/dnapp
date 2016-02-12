3a98dd8c6f299e787186bd3232823b7d663ea2c9
Arnold Sandoval <arnoldsandoval@me.com>
2016-02-12 14:24:43 -0800

    Adds StoryTableViewCell, changes StatusBarStyle, adds cell button animations

---
+++ DNApp/StoryTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  StoryTableViewCell.swift
+//  DNApp
+//
+//  Created by Arnold Sandoval on 2/12/16.
+//  Copyright © 2016 Arnold Sandoval. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var badgeImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var commentButton: SpringButton!
+    @IBOutlet weak var upvoteButton: SpringButton!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBAction func upvoteButtonDidTouch(sender: AnyObject) {
+        upvoteButton.animation = "pop"
+        upvoteButton.force = 3
+        upvoteButton.animate()
+    }
+    
+    @IBAction func commentButtonDidTouch(sender: AnyObject) {
+        commentButton.animation = "pop"
+        commentButton.force = 3
+        commentButton.animate()
+    }
+    
+}
